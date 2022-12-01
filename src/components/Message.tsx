471dae9124b7f27b1b2a39995ddef7587144a8b1
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-12-01 12:23:17 -0800

    add embeds

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,15 +1,24 @@
+import * as linkify from 'linkifyjs'
 import { BodyText, Link } from 'components/Text'
 import { Types } from 'ably'
+import Embed from 'react-embed'
 import Linkify from 'linkify-react'
 import classnames, {
+  alignItems,
   backgroundColor,
   borderRadius,
   display,
+  flexDirection,
   padding,
 } from 'classnames/tailwind'
 import isDarkColor from 'helpers/isDarkColor'
 import shortify from 'helpers/shortify'
 
+const container = classnames(
+  display('flex'),
+  flexDirection('flex-col'),
+  alignItems('items-stretch')
+)
 const pill = (isDark: boolean) =>
   classnames(
     display('inline'),
@@ -17,29 +26,34 @@ const pill = (isDark: boolean) =>
     borderRadius('rounded'),
     backgroundColor(isDark ? 'bg-indigo-300' : 'bg-indigo-900')
   )
-
 export default function ({ message }: { message: Types.Message }) {
   if (message.name !== 'chat-message') {
     return null
   }
+  const urls = linkify
+    .find(message.data.messageText)
+    .filter((u) => u.type === 'url')
   return (
-    <BodyText>
-      <div className={pill(isDarkColor(message.data.chatColor))}>
-        <span style={{ color: message.data.chatColor || undefined }}>
-          {message.data.username || shortify(message.data.address)}
-        </span>
-      </div>
-      :{' '}
-      <Linkify
-        options={{
-          defaultProtocol: 'https',
-          render: ({ attributes: { href }, content }) => {
-            return <Link href={href}>{content}</Link>
-          },
-        }}
-      >
-        {message.data.messageText}
-      </Linkify>
-    </BodyText>
+    <div className={container}>
+      <BodyText>
+        <div className={pill(isDarkColor(message.data.chatColor))}>
+          <span style={{ color: message.data.chatColor || undefined }}>
+            {message.data.username || shortify(message.data.address)}
+          </span>
+        </div>
+        :{' '}
+        <Linkify
+          options={{
+            defaultProtocol: 'https',
+            render: ({ attributes: { href }, content }) => {
+              return <Link href={href}>{content}</Link>
+            },
+          }}
+        >
+          {message.data.messageText}
+        </Linkify>
+      </BodyText>
+      {!!urls.length && <Embed isDark url={urls[0].href} />}
+    </div>
   )
 }
